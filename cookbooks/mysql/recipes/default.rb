00ad4b9a6efd4ffa90f7e3b83ccc6d480ce7a9d0
Junya Ishihara <junya@champierre.com>
2013-06-24 22:16:13 +0900

    add mysql recipe

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -7,23 +7,43 @@
 # All rights reserved - Do Not Redistribute
 #
 
-["mysql-server", "mysql", "mysql-devel"].each do |package_name|
-	package package_name do
-		version node['mysql']['version']
-		action :install
-	end
+ROOT_PASSWORD = 'chef'
+
+package_names = [
+  'MySQL-server',
+  'MySQL-client',
+  'MySQL-devel',
+  'MySQL-shared-compat'
+]
+
+package_names.each do |package_name|
+  rpm_file = "#{package_name}-5.6.10-1.el6.x86_64.rpm"
+
+  cookbook_file "/tmp/#{rpm_file}" do
+    mode 00644
+  end
+
+  package package_name do
+    action :install
+    source "/tmp/#{rpm_file}"
+  end
 end
 
-service "mysqld" do
-	supports :status => true, :restart => true, :reload => true
-	action [:enable, :start]
+service "mysql" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
+execute "setpassword" do
+  command "/usr/bin/mysqladmin -u root password " + ROOT_PASSWORD
+  action :nothing
 end
 
 template "my.cnf" do
-	path "/etc/my.cnf"
-	source "my.cnf.erb"
-	owner "root"
-	group "root"
-	mode 0644
-	notifies :restart, 'service[mysqld]'
+  path "/etc/my.cnf"
+  source "my.cnf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :restart, 'service[mysql]'
 end
